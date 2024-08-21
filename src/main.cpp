b4ad320ac4933491babcd0fad61069e4fc4d43d1
Adam Lawrence <adam@impostertwo.com>
2024-08-20 20:38:01 -0500

    ball bounce

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,64 @@
 
 using namespace std;
 
+class Ball {
+    public:
+    float x,y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw(){
+        DrawCircle(x, y, radius, WHITE);
+    }
+    void Update(){
+        x += speed_x;
+        y += speed_y;
+
+        if(y + radius >= GetScreenHeight() || y - radius <= 0){
+            speed_y *= -1;
+        }
+        if(x + radius >= GetScreenWidth() || x - radius <= 0){
+            speed_x *= -1;
+        }        
+    }
+};
+
+
+Ball ball;
+
 int main(){
     cout << "Starting Game! \n";
 
     //Make Window
-    const int screen_width = 1280;
-    const int screen_height = 800;
-    InitWindow(screen_width, screen_height, "My Pong Game");
+    const int width = 1280;
+    const int height = 800;
+    InitWindow(width, height, "My Pong Game");
     SetTargetFPS(60);
 
 
+    ball.radius = 20;
+    ball.x = width /2;
+    ball.y = height /2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+
     //Game Loop
-    while(!WindowShouldClose){
+    while(!WindowShouldClose()){
         BeginDrawing();
+
+        ball.Update();
+
+        ClearBackground(BLACK);
+        DrawLine(width /2, 0, width /2, height, WHITE);
+
+        ball.Draw();
+
+        //Draw Paddles
+        DrawRectangle(10, height /2 - 60, 25, 120, WHITE);
+        DrawRectangle(width-35, height /2 - 60, 25, 120, WHITE);
+
+        
 
         EndDrawing();
     }
